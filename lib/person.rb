8142d132381da34ae23ed156526b8da912e2795c
lior Shine <shinelior@gmail.com>
2020-06-02 07:24:10 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+
+class Person 
+  
+def initialize(name)
+   @name = name
+ end
+def name
+  @name
+ end
+ #def name = (new_name)
+  # @name = new_name
+#end
+end
+#adele_goldberg.name = "adele_goldberg"
+
+adele_goldberg = Person.new("adele_goldberg")
+
+
+lan_kay = Person.new("alan_kay")
+#lan_kay.new
